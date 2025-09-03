296fd5e384233ba87248d43b880588f1ba3926f6
Phindile Ntuli <67863432+PhindyChimele20@users.noreply.github.com>
2025-09-03 20:34:03 +0200

    Update script.sh

--- workflow/script.sh
+++ workflow/script.sh
@@ -1,1 +1,11 @@
+quast.py illumina_contigs.fasta.gz -o illumina_contigs
 
+prodigal -i illumina_contigs.fasta.gz -o illumina_contigs.gff -a illumina_proteins.fasta -d illumina_genes.fasta
+
+genomad end-to-end illumina_contigs.fasta.gz illumina_contigs_genomad.ouput /home/databases/genomad/genomad_db  -t 12
+
+dedupe.sh in=illumina_contigs_genomad.output/illumina_contigs_summary/illumina_contigs_virus.fna \
+          out=illumina_contigs_virus_deduped.fna \
+          minidentity=95
+
+cmscan --rfam --cut_ga --nohmmonly --tblout illumina_contigs.tblout --fmt 2 --clanin Rfam.clanin Rfam.cm illumina_contigs.fasta.gz > illumina_contigs.cmscan
